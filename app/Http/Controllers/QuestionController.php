ea00cdf70720b8d68591984a349df4ad40fe5515
rvstark17 <34938398+rvstark17@users.noreply.github.com>
2020-05-07 02:26:14 -0400

    add author info question date

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -15,9 +15,13 @@ class QuestionController extends Controller
     public function index()
     {
         //
-        $questions = Question::latest()->paginate(5);
-
+       // \DB::enableQueryLog();
+        $questions = Question::with('user')->latest()->paginate(5);
         return view('questions.index',compact('questions'));
+       // view('questions.index',compact('questions'))->render();
+
+       // dd(\DB::getQueryLog());
+
     }
 
     /**
