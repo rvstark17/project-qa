7481a7c5b88cdc0b9109c6586267800a2016afda
rvstark17 <34938398+rvstark17@users.noreply.github.com>
2020-05-09 17:26:35 -0400

    save question in database

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -23,7 +23,7 @@
                             <label for="question-title">
                                  Question Title
                             </label>
-                            <input type="text" name="title" id="questionTitle" class="form-control {{  $errors->has('title')  ? 'is-invalid' : '' }}">
+                            <input type="text" name="title" id="questionTitle" value="{{ old('title') }}" class="form-control {{  $errors->has('title')  ? 'is-invalid' : '' }}">
                             @if($errors->has('title'))
                                 <div class="invalid-feedback">
                                     <strong> {{ $errors->first('title') }}</strong>
@@ -34,7 +34,7 @@
                             <label for="question-body">
                                  Question Body
                             </label>
-                            <textarea name="body" id="questionBody" cols="30" rows="10" class="form-control {{  $errors->has('body')  ? 'is-invalid' : '' }}"></textarea>
+                            <textarea name="body" id="questionBody" cols="30" rows="10" class="form-control {{  $errors->has('body')  ? 'is-invalid' : '' }}"> {{ old('body') }}</textarea>
                             @if($errors->has('body'))
                                 <div class="invalid-feedback">
                                     <strong> {{ $errors->first('body') }}</strong>
