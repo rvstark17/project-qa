a829dd36427376abe2a15bae5d11af032206ee26
rvstark17 <34938398+rvstark17@users.noreply.github.com>
2020-05-10 18:02:24 -0400

    genereate answers seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,11 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         factory(App\User::class,3)->create()->each(function($user){
-            $user->questions()->saveMany(factory(App\Question::class,rand(1,5))->make());
+            $user->questions()->saveMany(factory(App\Question::class,rand(1,5))->make())->each(function($q){
+                $q->answers()->saveMany(factory(App\Answer::class,rand(1,5))->make());
+            });
         });
+        
+
     }
 }
