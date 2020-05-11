1ff84a9319c176b5786496a4a1c907199ce0f1a3
rvstark17 <34938398+rvstark17@users.noreply.github.com>
2020-05-10 23:03:45 -0400

    add vote design

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,9 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
+               <div class="card-body">
+               <div class="card-title">
                     <div class="d-flex align-items-center">
                          <h1>{{ $question->title }}</h1>
+                         
                          <div class="ml-auto">
                         <a href="{{ route('question.index') }}" class="btn btn-outline-secondary">Back to homepage</a>
                     </div>
@@ -15,21 +17,37 @@
                     
 
                 </div>
-
-                <div class="card-body">
-                   {!! $question->body_html !!}
-                   <div class="float-right">
-                        <span class="text-muted">questioned {{ $question->created_date }}</span>
-                        <div class="media mt-2">
-                            <a href="{{ $question->user->url }}" class="pr-2">
-                                <img src="{{ $question->user->avatar }}">
-                            </a>
-                            <div class="media-body mt-1">
-                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                <hr>
+                <div class="media">
+                    <div class="d-flex-column vote-controls">
+                        <a title="This question is useful" class="vote-up">
+                            <i class="fa fa-caret-up fa-3x"></i>
+                        </a>
+                        <span class="votes-count">123</span>
+                        <a title="This question is not useful" class="vote-down off">
+                        <i class="fa fa-caret-down fa-3x"></i>
+                        </a>
+                        <a  title="click to mark as favorite(Click again to undo)" class="favorite  mt-2">
+                        <i class="fa fa-star fa-2x"></i>
+                            <span class="favorite-count ">123</span>
+                        </a>
+                    </div>
+                    <div class="media-body">
+                        {!! $question->body_html !!}
+                     <div class="float-right">
+                            <span class="text-muted">questioned {{ $question->created_date }}</span>
+                            <div class="media mt-2">
+                                <a href="{{ $question->user->url }}" class="pr-2">
+                                    <img src="{{ $question->user->avatar }}">
+                                </a>
+                                <div class="media-body mt-1">
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+               </div>
             </div>
         </div>
     </div>
@@ -43,6 +61,19 @@
                     <hr>
                     @foreach($question->answers as $answer)
                         <div class="media">
+                                <div class="d-flex-column vote-controls">
+                                <a title="This Answer is useful" class="vote-up">
+                                    <i class="fa fa-caret-up fa-3x"></i>
+                                </a>
+                                <span class="votes-count">123</span>
+                                <a title="This Answer is not useful" class="vote-down off">
+                                <i class="fa fa-caret-down fa-3x"></i>
+                                </a>
+                                <a  title="Mark this question as best answer" class="vote-accept mt-2">
+                                <i class="fa fa-check fa-2x"></i>
+                                    
+                                </a>
+                            </div>
                             <div class="media-body">
                                 {!! $answer->body_html !!}
                                 <div class="float-right">
